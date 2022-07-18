4156a8ddeb32637380af0b1f9b08b6bdc99fddbb
Yike Gao <yikegao8@gmail.com>
2022-07-18 11:07:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path ="/Yike-portfolio" element = {<Layout />} >
+      <Route path ="/" element = {<Layout />} >
       <Route index element ={<Home />} /> 
       </Route>
 
